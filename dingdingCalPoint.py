ea9ff06ad37f434b35ef145450a85f605f3a1b99
DSG <you@example.com>
2020-08-06 19:34:30 +0800

    update punch

--- dingdingCalPoint.py
+++ dingdingCalPoint.py
@@ -10,13 +10,18 @@ from ROOT_DIR import ROOT_DIR, CAL_POINT_ARRAY
 
 
 def offWorkJob():
-    sleepTime = random.randint(0, 5)
+    sleepTime = random.randint(0, 100)
     print("sleepTime:" + str(sleepTime))
     time.sleep(sleepTime)
     devList = CAL_POINT_ARRAY
     devices = subprocess.Popen("adb devices", shell=True, stdout=subprocess.PIPE)
     devices.wait()
     devices = str(devices.stdout.read(), encoding="utf-8")
+    if "offline" in devices:
+        subprocess.Popen("adb kill-server", shell=True, stdout=subprocess.PIPE)
+        time.sleep(10)
+        subprocess.Popen("adb start-server", shell=True, stdout=subprocess.PIPE)
+        time.sleep(10)
     for dev in devList:
         print(ROOT_DIR + dev + "_offWork.sh " + dev)
         if dev in devices:
@@ -36,6 +41,11 @@ def go2WorkJob():
     devices.wait()
     devices = str(devices.stdout.read(), encoding="utf-8")
     print("devices:" + devices)
+    if "offline" in devices:
+        subprocess.Popen("adb kill-server", shell=True, stdout=subprocess.PIPE)
+        time.sleep(10)
+        subprocess.Popen("adb start-server", shell=True, stdout=subprocess.PIPE)
+        time.sleep(10)
     for dev in devList:
         print(ROOT_DIR + dev + "_Go2Work.sh " + dev)
         if dev in devices:
@@ -48,10 +58,12 @@ def go2WorkJob():
 
 if __name__ == '__main__':
     #adb devices
-
-    schedule.every().day.at("21:15").do(offWorkJob)
-    schedule.every().day.at("08:40").do(go2WorkJob)
-
+    devices = subprocess.Popen("adb devices", shell=True, stdout=subprocess.PIPE)
+    devices.wait()
+    devices = str(devices.stdout.read(), encoding="utf-8")
+    print("devices:" + devices)
+    schedule.every().day.at("19:35").do(offWorkJob)
+    schedule.every().day.at("08:35").do(go2WorkJob)
     while True:
         schedule.run_pending()
         print("wait 1s  current time:" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
