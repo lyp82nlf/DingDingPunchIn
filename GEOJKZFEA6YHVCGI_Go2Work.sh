e1e88cc98e7441be2b291d1a8e13eb80a7206488
qiuling <qiuling@baijiayun.com>
2021-03-11 18:30:28 +0800

    fix 增加截图

--- GEOJKZFEA6YHVCGI_Go2Work.sh
+++ GEOJKZFEA6YHVCGI_Go2Work.sh
@@ -12,20 +12,26 @@ sleep 4s
 adb -s $1 shell input swipe 300 1000 300 500
 echo 解锁 >> $currentlog
 sleep 10s
+sh screencap.sh $1 解锁完成
 adb -s $1 shell am start -n com.alibaba.android.rimet/.biz.LaunchHomeActivity
 echo 开启钉钉 >> $currentlog
 sleep 30s
+sh screencap.sh $1 开启钉钉
 adb -s $1 shell input tap 531  1842 #点击百家云图标
 echo 点击百家云图标 >> $currentlog
 sleep 30s
+sh screencap.sh $1 点击百家云图标
 adb -s $1 shell input tap 150  975 #点击考勤打卡图标
 echo 点击考勤打卡图标 >> $currentlog
 sleep 30s
+sh screencap.sh $1 点击考勤打卡图标
 adb -s $1 shell input tap 546 1144 #点击考勤打卡
 echo 点击考勤打卡 >> $currentlog
 sleep 30s
+sh screencap.sh $1 点击考勤打卡
 adb -s $1 shell am force-stop com.alibaba.android.rimet
 echo 关闭钉钉 >> $currentlog
 sleep 30s
+sh screencap.sh $1 关闭钉钉
 adb -s $1 shell input keyevent 223 #熄灭屏幕
 echo 熄灭屏幕 >> $currentlog
